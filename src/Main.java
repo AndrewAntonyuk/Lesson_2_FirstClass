501562ca4762d71d1b609a99139f64b06d70c65b
AndrewAntonyuk <antonyuk.andrij@gmail.com>
2022-10-10 23:27:13 +0300

    Add empty employee to the main

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ public class Main {
         Employee employee = new Employee("sdsdc", "acsdc", "sdcsdc", "ijcdj", "sdcsdcsdc", "sdcsdc", 45);
         System.out.println(employee.toString());
 
+        Employee employee1 = new Employee();
+        System.out.println(employee1.toString());
+
         Car car = new Car();
         car.start();
     }
